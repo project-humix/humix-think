4bb4924bc31d6a07868524a76ffbceb97d3b42cd
wettingchou <wtchou1209@gmail.com>
2015-10-13 02:25:28 +0800

    bug fixing

--- nodes/core/sense/sense.js
+++ nodes/core/sense/sense.js
@@ -4,21 +4,35 @@ module.exports = function(RED) {
 
     function SenseEvent(n) {
         RED.nodes.createNode(this,n);
-        var senseId = n.senseid;
-        var node = this;
+        var senseId = n.senseid,
+            eventType = n.eventtype,
+            node = this;
 
         RED.comms.subscribe(senseId, function(data) {
-            node.send({
-                payload: data
-            });
+            try {
+                var event;
+                if (typeof data === 'string') {
+                    event = JSON.parse(data);
+                } else {
+                    event = data;
+                }
+                if (event.topic === eventType) {
+                    node.send({
+                        payload: event.message
+                    });
+                }
+            } catch (e) {
+                node.error('Error: '+e);
+            }
         });
     }
     RED.nodes.registerType("sense event", SenseEvent);
 
     function SenseCommand(n) {
         RED.nodes.createNode(this,n);
-        var senseId = n.senseid;
-        var node = this;
+        var senseId = n.senseid,
+            commandType = n.commandtype,
+            node = this;
 
         node.on('input', function(msg) {
             if (!msg.payload) {
@@ -26,7 +40,16 @@ module.exports = function(RED) {
                 return;
             }
 
-            RED.comms.publish(senseId, msg.payload, true);
+            var message = {
+                header: {
+                    type: 'modules'
+                },
+                payload: {
+                    type: commandType,
+                    command: msg.payload
+                }
+            };
+            RED.comms.publish(senseId, message, true);
         });
     }
     RED.nodes.registerType("sense command", SenseCommand);

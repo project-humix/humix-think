05600435483c8858dd11603509fb33ca6575acb9
jeffffrey@gmail.com <jeffffrey@gmail.com>
2016-05-27 13:32:17 +0800

    add text-to-speech support

--- deployThink.sh
+++ deployThink.sh
@@ -7,6 +7,7 @@ cf create-service cloudantNoSQLDB Shared Humix-Cloudant-Service
 cf create-service dialog standard Humix-Dialog-Service
 cf create-service natural_language_classifier standard Humix-NLC-Service
 cf create-service speech_to_text standard Humix-Speech-Service
+cf create-service text_to_speech standard Humix-TTS-Service
 
 # deploy application
 

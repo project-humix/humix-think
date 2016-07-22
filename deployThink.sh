7de2cc77323271751efaa308f1dc8238832f45bf
jeffffrey@gmail.com <jeffffrey@gmail.com>
2016-07-23 00:26:40 +0800

    change default bluemix service binding

--- deployThink.sh
+++ deployThink.sh
@@ -4,10 +4,9 @@ cf login
 
 # create service
 cf create-service cloudantNoSQLDB Shared Humix-Cloudant-Service
-cf create-service dialog standard Humix-Dialog-Service
-cf create-service natural_language_classifier standard Humix-NLC-Service
+cf create-service conversation standard Humix-Conversation-Service
 cf create-service speech_to_text standard Humix-Speech-Service
-cf create-service text_to_speech standard Humix-TTS-Service
+#cf create-service text_to_speech standard Humix-TTS-Service
 
 # deploy application
 

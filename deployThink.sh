708ffd3831f91ada47b4c0e894c1919e7d1c6754
Jeffrey Liu <jeffffrey@gmail.com>
2016-11-26 17:16:25 +0800

    Change cloudant service type

--- deployThink.sh
+++ deployThink.sh
@@ -4,7 +4,7 @@
 cf login -a https://api.ng.bluemix.net
 
 # create service
-cf create-service cloudantNoSQLDB Shared Humix-Cloudant-Service
+cf create-service cloudantNoSQLDB Lite Humix-Cloudant-Service
 cf create-service conversation standard Humix-Conversation-Service
 cf create-service speech_to_text standard Humix-Speech-Service
 #cf create-service text_to_speech standard Humix-TTS-Service

2c9c231835acb3f3e7f988649a75c5d6d629095f
Jeffrey Liu <jeffffrey@gmail.com>
2017-01-06 19:42:30 +0800

    Add IBM Copyright

--- deployThink.sh
+++ deployThink.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# © Copyright IBM Corp. 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 # Set Bluemix API Endpoint. You will need this change this if your account use differnt default api endpoint
 cf login -a https://api.ng.bluemix.net
 
@@ -10,5 +26,4 @@ cf create-service speech_to_text standard Humix-Speech-Service
 #cf create-service text_to_speech standard Humix-TTS-Service
 
 # deploy application
-
 cf push
--- frontend/src/app/components/deviceList/deviceList.service.js
+++ frontend/src/app/components/deviceList/deviceList.service.js
@@ -1,3 +1,20 @@
+/**
+* © Copyright IBM Corp. 2016
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+
 (function() {
   'use strict';
 
@@ -19,7 +36,7 @@
         });
       }, function errorCallback(response) {
         $log.info(response);
-    });      
+    });
 
     this.getDevices = getDevices;
     this.setDevice = setDevice;
@@ -39,16 +56,16 @@
           $log.info(response);
         }, function errorCallback(response) {
           $log.info(response);
-      });      
+      });
       devices[senseId] = iconId;
     }
 
-    function delDevice(senseId) {      
+    function delDevice(senseId) {
       // call the backend api here to delete the data
       $http({
         method: 'DELETE',
         url: 'api/devices/'+ senseId
-      });      
+      });
 
       delete devices[senseId];
     }
